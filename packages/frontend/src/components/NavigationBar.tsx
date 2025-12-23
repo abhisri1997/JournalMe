d751ab3f90acd8100210fc78e1254973883494e2
Abhinav <abhisri1997@gmail.com>
2025-12-24 02:01:04 +0530

    fix: mobile navs

--- packages/frontend/src/components/NavigationBar.tsx
+++ packages/frontend/src/components/NavigationBar.tsx
@@ -16,53 +16,65 @@ export default function NavigationBar({
   onMenuToggle,
 }: NavigationBarProps) {
   return (
-    <nav className='site-nav' role='navigation' aria-label='Main navigation'>
-      <div className='nav-left'>
+    <>
+      <div className='mobile-top-bar'>
         <div className='logo' aria-label='JournalMe logo'>
           <JournalIcon className='logo-icon' />
           <span className='logo-text'>JournalMe</span>
         </div>
-        <div
-          id='primary-navigation'
-          className={`nav-links ${menuOpen ? "open" : ""}`}
-          aria-label='Primary'
-        >
-          <NavLink to='/' end aria-label='Home'>
-            <HomeIcon className='icon' /> Home
-          </NavLink>
-          <NavLink to='/journal' aria-label='Journal'>
-            <JournalIcon className='icon' /> Journal
-          </NavLink>
-          {isAuthenticated && (
-            <NavLink to='/community' aria-label='Community'>
-              <JournalIcon className='icon' /> Community
-            </NavLink>
-          )}
-          {!isAuthenticated && (
-            <>
-              <NavLink to='/login' aria-label='Login'>
-                <UserIcon className='icon' /> Login
-              </NavLink>
-              <NavLink to='/register' aria-label='Register'>
-                <UserIcon className='icon' /> Register
-              </NavLink>
-            </>
-          )}
+        <div className='mobile-top-actions'>
+          {isAuthenticated ? <ProfileMenu /> : <ThemeToggle />}
         </div>
       </div>
-      <div className='nav-right'>
-        <button
-          className='menu-button'
-          aria-label='Toggle menu'
-          aria-controls='primary-navigation'
-          aria-expanded={menuOpen}
-          onClick={onMenuToggle}
-        >
-          Menu
-        </button>
-        {!isAuthenticated && <ThemeToggle />}
-        {isAuthenticated && <ProfileMenu />}
-      </div>
-    </nav>
+
+      <nav className='site-nav' role='navigation' aria-label='Main navigation'>
+        <div className='nav-left'>
+          <div className='logo' aria-label='JournalMe logo'>
+            <JournalIcon className='logo-icon' />
+            <span className='logo-text'>JournalMe</span>
+          </div>
+          <div
+            id='primary-navigation'
+            className={`nav-links ${menuOpen ? "open" : ""}`}
+            aria-label='Primary'
+          >
+            <NavLink to='/journal' aria-label='Journal'>
+              <JournalIcon className='icon' /> Journal
+            </NavLink>
+            <NavLink to='/' end aria-label='Home'>
+              <HomeIcon className='icon' /> Home
+            </NavLink>
+            {isAuthenticated && (
+              <NavLink to='/community' aria-label='Community'>
+                <JournalIcon className='icon' /> Community
+              </NavLink>
+            )}
+            {!isAuthenticated && (
+              <>
+                <NavLink to='/login' aria-label='Login'>
+                  <UserIcon className='icon' /> Login
+                </NavLink>
+                <NavLink to='/register' aria-label='Register'>
+                  <UserIcon className='icon' /> Register
+                </NavLink>
+              </>
+            )}
+          </div>
+        </div>
+        <div className='nav-right'>
+          <button
+            className='menu-button'
+            aria-label='Toggle menu'
+            aria-controls='primary-navigation'
+            aria-expanded={menuOpen}
+            onClick={onMenuToggle}
+          >
+            Menu
+          </button>
+          {!isAuthenticated && <ThemeToggle />}
+          {isAuthenticated && <ProfileMenu />}
+        </div>
+      </nav>
+    </>
   );
 }
